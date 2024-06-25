c3af9c384256a700c7b3f7c1fa39de55dfd85781
sainadth <sainadth1123434@gmail.com>
2024-06-26 00:22:19 +0530

    Update 1038.cpp

--- 1038.cpp
+++ 1038.cpp
@@ -12,6 +12,15 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
     TreeNode* bstToGst(TreeNode* root) {
